0810929adbb668cdc4f524be722bda352ebcb17a
Chester How <chesterhow@gmail.com>
2016-08-28 11:55:07 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Messenger from './lib/messenger';
+
+const bot = new Messenger();
+
+bot.listen().then(() => { console.log('listen'); });
---
+++ src/lib/message.js
@@ -0,0 +1,18 @@
+export default class Message {
+  constructor(msg) {
+    this.from = msg.from;
+    this.text = msg.text;
+    this.user = msg.user;
+  }
+
+  static mapMessage(msg) {
+    return {
+      from: msg.from.id,
+      text: msg.text,
+      user: {
+        firstName: msg.from.first_name,
+        lastName: msg.from.last_name,
+      }
+    };
+  }
+}
