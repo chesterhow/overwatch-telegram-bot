6572fc4cfb04bdbb48c57882a054c5bc9330a421
Chester How <chesterhow@gmail.com>
2016-08-28 17:03:48 +0800

    Disable webhooks

--- src/lib/messenger.js
+++ src/lib/messenger.js
@@ -8,12 +8,14 @@ const validator = new Validator();
 
 export default class Messenger {
   constructor() {
-    if (process.env.NODE_ENV === 'production') {
-      this.bot = new TelegramBot(config.telegram.token, { webHook: { port: config.telegram.port, host: config.telegram.host } });
-      this.bot.setWebHook(`${config.telegram.externalUrl}:433/bot${config.telegram.token}`);
-    } else {
-      this.bot = new TelegramBot(config.telegram.token, { polling: true });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   this.bot = new TelegramBot(config.telegram.token, { webHook: { port: config.telegram.port, host: config.telegram.host } });
+    //   this.bot.setWebHook(`${config.telegram.externalUrl}:433/bot${config.telegram.token}`);
+    // } else {
+    //   this.bot = new TelegramBot(config.telegram.token, { polling: true });
+    // }
+
+    this.bot = new TelegramBot(config.telegram.token, { polling: true });
 
     this.handleText = this.handleText.bind(this);
   }
