600b21983c08bc3cdbb774b2648aeb69fc812191
Chester How <chesterhow@gmail.com>
2016-08-29 13:46:41 +0800

    Rename procfile

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import Messenger from './lib/messenger';
 
 const bot = new Messenger();
 
-console.log(process.env.APP_PORT);
-bot.listen(process.env.APP_PORT || 443).then(() => { console.log('listen'); });
+bot.listen().then(() => { console.log('listen'); });
