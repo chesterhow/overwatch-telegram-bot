93bc329741e93e1fb7f56efe0944ae1d8ed3d523
Savely Krasovsky <savely@krasovsky.me>
2016-12-08 22:44:25 +0300

    Add console support, p. 2

--- src/services/fetchStats.js
+++ src/services/fetchStats.js
@@ -4,8 +4,8 @@ import { API_URL, HTTP_HEADERS } from '../constants';
 
 const headers = HTTP_HEADERS;
 
-const fetchStats = (battletag) => {
-  const url = `${API_URL}/${hashToHypen(battletag)}/blob`;
+const fetchStats = (battletag,platform) => {
+  const url = `${API_URL}/${hashToHypen(battletag)}/blob?platform=${platform}`;
 
   return fetch(encodeURI(url), { headers })
     .then((response) => response.json())
